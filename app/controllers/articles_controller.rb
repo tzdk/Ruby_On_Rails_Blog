7da5ba027c41df3bcbf4adbd77f68ce7c10419a3
tzdk <yefeng_dk@163.com>
2016-08-04 22:36:03 +0800

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,16 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
   
   def create
     @article = Article.new(article_params)
    
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end 
   end
  
   private def article_params
@@ -15,5 +19,30 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
   end
 end
