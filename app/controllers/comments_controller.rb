e7ffb6e34a458afecbc9b061db1d15598eb04981
tzdk <yefeng_dk@163.com>
2016-09-06 14:38:16 +0800

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,8 @@ class CommentsController < ApplicationController
     redirect_to article_path(@article)
   end
  
+ 
+ 
    def destroy
       @article = Article.find(params[:article_id])
       @comment = @article.comments.find(params[:id])
